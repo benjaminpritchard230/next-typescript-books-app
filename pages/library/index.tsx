2a60cbbc690df0d549d7fb20dbca3998d28549a4
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-22 23:17:52 +0100

    Create library page

---
+++ pages/library/index.tsx
@@ -0,0 +1,41 @@
+import { useGetBooksQuery } from "@/features/api/apiSlice";
+import { RootState } from "@/store/store";
+import React from "react";
+import { useSelector } from "react-redux";
+
+type Props = {};
+
+interface IBook {
+  isRead: boolean;
+  _id: string;
+  user: string;
+  isbn: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  title?: string;
+}
+
+const Library = (props: Props) => {
+  const auth = useSelector((state: RootState) => state.auth);
+
+  const { data: bookData, error, isError } = useGetBooksQuery({});
+
+  if (bookData) {
+    console.log(bookData);
+  }
+
+  return (
+    <>
+      <h3>My library</h3>
+
+      <ul>
+        {bookData?.map((book: IBook) => {
+          return <li>{book.title}</li>;
+        })}
+      </ul>
+    </>
+  );
+};
+
+export default Library;
