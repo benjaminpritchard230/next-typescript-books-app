709aeda57a3908cd0f4ed0347996258412ca8634
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-22 01:36:39 +0100

    Get register page working

--- pages/register/index.tsx
+++ pages/register/index.tsx
@@ -1,9 +1,120 @@
-import React from "react";
+import { useRegisterMutation } from "@/features/api/apiSlice";
+import { setCredentials } from "@/features/auth/authSlice";
+import { AppDispatch, RootState } from "@/store/store";
+import React, { useEffect, useState } from "react";
+import { FaUser } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 type Props = {};
 
+export interface FormData {
+  name?: string;
+  email: string;
+  password: string;
+  password2?: string;
+}
+
 const Register = (props: Props) => {
-  return <div>Register</div>;
+  const dispatch = useDispatch<AppDispatch>();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const [register] = useRegisterMutation();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+    console.log(formData);
+  };
+
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    if (password !== password2) {
+      console.log("Passwords don't match");
+    } else
+      try {
+        const user = await register(formData).unwrap();
+        dispatch(setCredentials(user));
+      } catch (err) {
+        console.log(err);
+      }
+  };
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password2"
+              name="password2"
+              value={password2}
+              onChange={onChange}
+              placeholder="Confirm password"
+            />
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Register;
