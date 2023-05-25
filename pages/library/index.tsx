e294b90787314f753432b7f943450fc32813ae76
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-25 23:34:49 +0100

    Use book card component to show lib books

--- pages/library/index.tsx
+++ pages/library/index.tsx
@@ -1,21 +1,12 @@
+import BookCard from "@/components/BookCard";
 import { useGetBooksQuery } from "@/features/api/apiSlice";
 import { RootState } from "@/store/store";
+import { IUserBook } from "@/types/userBooks";
 import Link from "next/link";
 import React from "react";
 import { useSelector } from "react-redux";
 
 type Props = {};
-
-interface IBook {
-  isRead: boolean;
-  _id: string;
-  user: string;
-  isbn: string;
-  createdAt: string;
-  updatedAt: string;
-  __v: number;
-  title?: string;
-}
 
 const Library = (props: Props) => {
   const auth = useSelector((state: RootState) => state.auth);
@@ -31,12 +22,8 @@ const Library = (props: Props) => {
       <h3>My library</h3>
 
       <ul>
-        {bookData?.map((book: IBook) => {
-          return (
-            <Link key={book._id} href={`/book/${book.isbn}`}>
-              <li>{book.title}</li>
-            </Link>
-          );
+        {bookData?.map((book: IUserBook) => {
+          return <BookCard key={book._id} book={book} />;
         })}
       </ul>
     </>
