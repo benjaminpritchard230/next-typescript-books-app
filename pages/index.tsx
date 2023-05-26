5e316d7cc31cd728787dee7ba89076604cf56244
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-26 23:32:03 +0100

    Add search function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 type Props = {};
 
 const Homepage = (props: Props) => {
-  return <div>Homepage</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const router = useRouter();
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (searchText) {
+      router.push(`/search?searchText=${searchText}`);
+    }
+  };
+  return (
+    <>
+      <div>
+        <h2>Search for books by title</h2>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <div className={"input-container"}>
+            <input
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={searchText}
+              onChange={(e) => {
+                setSearchText(e.target.value);
+              }}
+            />
+            <button className="button" type="submit">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Homepage;
