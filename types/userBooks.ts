753c6d4abb5f97ec633fd666174f617d937636ef
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-27 02:32:15 +0100

    Update type for userBooks

--- types/userBooks.ts
+++ types/userBooks.ts
@@ -4,9 +4,87 @@ export interface IUserBook {
   _id: string;
   user: string;
   isbn: string;
+  data: Data;
+  isRead: boolean;
   createdAt: string;
   updatedAt: string;
   __v: number;
-  isRead: boolean;
-  title?: string;
+}
+
+export interface Data {
+  other_titles: string[];
+  publishers: string[];
+  number_of_pages: number;
+  table_of_contents: TableOfContent[];
+  ia_box_id: string[];
+  series: string[];
+  pagination: string;
+  covers: number[];
+  physical_format: string;
+  key: string;
+  authors: Author[];
+  publish_places: string[];
+  description: string;
+  genres: string[];
+  source_records: string[];
+  title: string;
+  identifiers: Identifiers;
+  languages: Language[];
+  subjects: string[];
+  publish_date: string;
+  copyright_date: string;
+  by_statement: string;
+  works: Work[];
+  type: Type2;
+  ocaid: string;
+  isbn_10: string[];
+  isbn_13: string[];
+  lccn: string[];
+  oclc_numbers: string[];
+  lc_classifications: string[];
+  latest_revision: number;
+  revision: number;
+  created: Created;
+  last_modified: LastModified;
+}
+
+export interface TableOfContent {
+  level: number;
+  title: string;
+  type: Type;
+}
+
+export interface Type {
+  key: string;
+}
+
+export interface Author {
+  key: string;
+}
+
+export interface Identifiers {
+  alibris_id: string[];
+  goodreads: string[];
+}
+
+export interface Language {
+  key: string;
+}
+
+export interface Work {
+  key: string;
+}
+
+export interface Type2 {
+  key: string;
+}
+
+export interface Created {
+  type: string;
+  value: string;
+}
+
+export interface LastModified {
+  type: string;
+  value: string;
 }
