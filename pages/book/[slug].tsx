8610bf4590ab9028664e696d6e54244a891ed46c
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-27 02:30:08 +0100

    Use data from custom api rather than openlibrary

--- pages/book/[slug].tsx
+++ pages/book/[slug].tsx
@@ -1,4 +1,4 @@
-import { useGetBookQuery } from "@/features/api/apiSlice";
+import { useGetBookQuery, useGetBooksQuery } from "@/features/api/apiSlice";
 import { RootState } from "@/store/store";
 import { IBookData } from "@/types/books";
 import { GetServerSideProps, GetStaticProps } from "next";
@@ -6,29 +6,23 @@ import { useRouter } from "next/router";
 import React from "react";
 import { useSelector } from "react-redux";
 
-type Props = {
-  data: IBookData;
-};
+type Props = {};
 
-const BookDetails = ({ data }: Props) => {
+const BookDetails = (props: Props) => {
   const router = useRouter();
   const auth = useSelector((state: RootState) => state.auth);
 
-  const {
-    data: bookData,
-    error,
-    isError,
-  } = useGetBookQuery({ isbn: router.query.slug });
+  const { data: booksData, error, isError } = useGetBooksQuery({});
 
-  console.log(data);
-  if (!error) {
-    console.log(bookData);
-  }
+  // const book = booksData.filter((book) => book._id === router.query.slug);
+  const id = router.query.slug;
+  console.log(booksData!.filter((book) => book._id === id));
+  const book = booksData!.filter((book) => book._id === id);
 
   return (
     <>
-      <p>Showing details for &quot;{data.title}&quot;</p>
-      <p>ISBN: {router.query.slug}</p>
+      {/* <p>Showing details for &quot;{book.title}&quot;</p> */}
+      <p>ISBN: {book[0].data.isbn_13 || book[0].data.isbn_10}</p>
       <p>
         {error
           ? "You have not added this book to your library."
@@ -39,16 +33,3 @@ const BookDetails = ({ data }: Props) => {
 };
 
 export default BookDetails;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  {
-    const response = await fetch(
-      `https://openlibrary.org/isbn/${context.params!.slug}.json`
-    );
-    const data: IBookData = await response.json();
-
-    return {
-      props: { data },
-    };
-  }
-};
