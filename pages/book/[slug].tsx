959d91259304d31857e4f2190cca3e4b94918273
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-28 05:05:22 +0100

    Add conditional display of book data if present

--- pages/book/[slug].tsx
+++ pages/book/[slug].tsx
@@ -12,22 +12,29 @@ const BookDetails = (props: Props) => {
   const router = useRouter();
   const auth = useSelector((state: RootState) => state.auth);
 
-  const { data: booksData, error, isError } = useGetBooksQuery({});
+  const { data: booksData, error, isError } = useGetBooksQuery();
 
   // const book = booksData.filter((book) => book._id === router.query.slug);
   const id = router.query.slug;
   console.log(booksData!.filter((book) => book._id === id));
-  const book = booksData!.filter((book) => book._id === id);
+  const book = booksData!.filter((book) => book._id === id)[0];
 
   return (
     <>
-      {/* <p>Showing details for &quot;{book.title}&quot;</p> */}
-      <p>ISBN: {book[0].data.isbn_13 || book[0].data.isbn_10}</p>
-      <p>
-        {error
-          ? "You have not added this book to your library."
-          : "This book is in your library."}
-      </p>
+      {book.data.title && (
+        <p>Showing details for &quot;{book.data.title}&quot;</p>
+      )}
+      {book.data.isbn_10[0] && <p>ISBN: {book.data.isbn_10[0]}</p>}
+      {book.data.isbn_13 && <p>ISBN: {book.data.isbn_13}</p>}
+
+      {book.data.physical_format && <p>Format: {book.data.physical_format}</p>}
+      {book.data.copyright_date && (
+        <p>Copyright date: {book.data.copyright_date}</p>
+      )}
+      {book.data.publish_date && <p>Published: {book.data.publish_date}</p>}
+      {book.data.publishers && <p>Publisher: {book.data.publishers}</p>}
+      {book.data.description && <p>Description: {book.data.description}</p>}
+      {book.data.covers}
     </>
   );
 };
