d6b17bcd7cb5922265eb3d2ff5159f85070c5c56
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-05-28 23:05:44 +0100

    Add delete, markread, and add note to book page

--- pages/book/[slug].tsx
+++ pages/book/[slug].tsx
@@ -1,9 +1,14 @@
-import { useGetBookQuery, useGetBooksQuery } from "@/features/api/apiSlice";
+import {
+  useDeleteBookMutation,
+  useGetBookQuery,
+  useGetBooksQuery,
+  useUpdateBookMutation,
+} from "@/features/api/apiSlice";
 import { RootState } from "@/store/store";
 import { IBookData } from "@/types/books";
 import { GetServerSideProps, GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 type Props = {};
@@ -13,11 +18,42 @@ const BookDetails = (props: Props) => {
   const auth = useSelector((state: RootState) => state.auth);
 
   const { data: booksData, error, isError } = useGetBooksQuery();
+  const [deleteBook] = useDeleteBookMutation();
+  const [updateBook] = useUpdateBookMutation();
 
   // const book = booksData.filter((book) => book._id === router.query.slug);
   const id = router.query.slug;
-  console.log(booksData!.filter((book) => book._id === id));
   const book = booksData!.filter((book) => book._id === id)[0];
+
+  const [note, setNote] = useState("");
+
+  const handleDelete = () => {
+    deleteBook({ id: book._id });
+  };
+
+  const handleMarkRead = () => {
+    const updatedBook = {
+      ...book,
+      isRead: !book.isRead,
+    };
+    updateBook(updatedBook);
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNote(event.target.value);
+  };
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+    event.preventDefault();
+    // Here, you can perform any action with the note, such as sending it to a server or updating a state in your parent component.
+    console.log("Note:", note);
+    const updatedBook = {
+      ...book,
+      notes: note,
+    };
+    updateBook(updatedBook);
+    setNote(""); // Clear the input field after submitting the note
+  };
 
   return (
     <>
@@ -34,7 +70,31 @@ const BookDetails = (props: Props) => {
       {book.data.publish_date && <p>Published: {book.data.publish_date}</p>}
       {book.data.publishers && <p>Publisher: {book.data.publishers}</p>}
       {book.data.description && <p>Description: {book.data.description}</p>}
-      {book.data.covers}
+      {book.isRead ? "You have read this book" : "You haven't read this book"}
+      {book.notes && <p>Your notes: {book.notes}</p>}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={note}
+          onChange={handleChange}
+          placeholder="Add a note..."
+        />
+        <button type="submit">Add</button>
+      </form>
+      <button
+        onClick={() => {
+          handleDelete();
+        }}
+      >
+        Remove from library
+      </button>
+      <button
+        onClick={() => {
+          handleMarkRead();
+        }}
+      >
+        {book.isRead ? "Mark book as unread" : "Mark book as read"}
+      </button>
     </>
   );
 };
