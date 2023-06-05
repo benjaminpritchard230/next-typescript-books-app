e43371d4610754167e974fc61af85aeb1db8da4f
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-06-05 03:25:38 +0100

    Remake footer component with links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,40 +20,45 @@ const Footer = (props: Props) => {
   };
 
   return (
-    <ul>
-      <li>
-        <Link href="/">Home</Link>
-      </li>
-
-      <li>
-        <Link href="/register">Register</Link>
-      </li>
-
-      <li>
-        <Link href="/add-book">Add book</Link>
-      </li>
-      <li>
-        <Link href="/library">Library</Link>
-      </li>
-      <br />
-      {token ? (
-        <button
-          onClick={() => {
-            handleLogout();
-          }}
-        >
-          Logout
-        </button>
-      ) : (
-        <button
-          onClick={() => {
-            handleLogin();
-          }}
-        >
-          Login
-        </button>
-      )}
-    </ul>
+    <footer
+      className="bg-gray-900
+    text-3xl text-white text-center
+    fixed
+    inset-x-0
+    bottom-0
+    p-1
+    hidden
+    md:flex
+    "
+    >
+      <div className="container mx-auto px-4">
+        <ul className="flex flex-wrap items-center justify-between">
+          <li className="text-gray-300 text-sm px-2 py-1">
+            Ben Pritchard 2023
+          </li>
+          <li className="text-gray-300 text-sm px-2 py-1">
+            <a
+              href="https://openlibrary.org/"
+              className="hover:text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Powered by Open Library
+            </a>
+          </li>
+          <li className="text-gray-300 text-sm px-2 py-1">
+            <a
+              href="https://github.com/benjaminpritchard230/next-typescript-books-app"
+              className="hover:text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
   );
 };
 
