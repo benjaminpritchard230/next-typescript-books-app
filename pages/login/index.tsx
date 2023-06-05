43213b18ed6192517a54b6241f7f639fb78e7832
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-06-05 03:27:19 +0100

    Change appearance of input to match

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -101,7 +101,7 @@ const Login = (props: Props) => {
           <div className="mb-4">
             <button
               type="submit"
-              className="w-full p-2 bg-blue-500 text-white rounded"
+              className="w-full p-2 bg-indigo-500 text-white rounded"
             >
               Submit
             </button>
