1f4f7830d5fc02b5439b2ec28d8fa57103b16d05
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-06-07 21:13:03 +0100

    Add delete/read buttons to book card

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -5,6 +5,7 @@ import {
 import { IUserBook } from "@/types/userBooks";
 import Link from "next/link";
 import React, { useState } from "react";
+import { AiOutlineCheckSquare, AiOutlineCloseSquare } from "react-icons/ai";
 
 type Props = {
   book: IUserBook;
@@ -36,6 +37,51 @@ const BookCard = ({ book }: Props) => {
   return (
     <>
       <li className="bg-white rounded-lg p-4 shadow-md">
+        <div className="flex justify-end mb-3">
+          {" "}
+          <button
+            onClick={() => {
+              handleDelete();
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-gray-900 hover:text-red-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+          <button
+            className="ml-1"
+            onClick={() => {
+              handleMarkRead();
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              className="w-6 h-6 text-gray-900 hover:text-green-500"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+        </div>
+
         <Link key={book._id} href={`/book/${book._id}`}>
           <div className="flex flex-col items-center">
             <img
