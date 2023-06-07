eeab3c43180392d18ab854df51470907d132246d
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-06-07 02:16:54 +0100

    Add Head to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { ReactNode } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
@@ -7,6 +8,28 @@ type Props = { children: ReactNode };
 const Layout = ({ children }: Props) => {
   return (
     <>
+      <Head>
+        <title>ShelfSpace</title>
+        <meta name="Layout" content="ShelfSpace" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <Navbar />
       <main>{children}</main>
       <Footer />
