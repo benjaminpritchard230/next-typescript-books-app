06389687f3878624be11c9221d941160f7898bd3
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-06-09 02:38:45 +0100

    Add book logo to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,13 +22,30 @@ const Navbar = (props: Props) => {
   const capitaliseString = (str: string) => {
     return str.charAt(0).toUpperCase() + str.slice(1);
   };
+
   return (
     <nav className="bg-gray-900 py-4">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between">
-          <Link href="/" className="text-white text-lg font-semibold">
-            ShelfSpace
-          </Link>
+          <div className="flex items-center">
+            <Link href="/" className="flex text-white text-lg font-semibold">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+                />
+              </svg>
+              <span className="ml-1 hidden sm:flex ">ShelfSpace</span>
+            </Link>
+          </div>
 
           <div className="flex">
             {!token ? (
@@ -58,7 +75,7 @@ const Navbar = (props: Props) => {
                 >
                   Logout
                 </a>
-                <p className="ml-4 px-2 py-1 text-gray-300 ">
+                <p className="ml-4 px-2 py-1 text-gray-300">
                   {capitaliseString(name)}
                 </p>
               </>
