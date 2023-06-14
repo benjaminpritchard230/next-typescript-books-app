89932830bd281d1a452ee14eb56845e9b7ae65a0
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2023-06-14 22:39:56 +0100

    Add loading spinner and add clear btn to filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,17 +27,13 @@ const Homepage = (props: Props) => {
 
   const [errorMessage, setErrorMessage] = useState("");
 
-  const [addBook, { isLoading, error, isSuccess }] = useAddBookMutation();
-
-  if (error) {
-    console.log(error);
-  }
-
   const [formData, setFormData] = useState<IAddBookFormData>({
     isbn: "",
   });
 
   const [bookTitle, setBookTitle] = useState("");
+
+  const [addBook, { isLoading, error, isSuccess }] = useAddBookMutation();
 
   const { isbn } = formData;
 
@@ -246,10 +242,32 @@ const Homepage = (props: Props) => {
           />
           <button
             type="submit"
-            className="bg-indigo-500 hover:bg-indigo-400 text-white py-2 px-4 rounded-r-lg disabled:opacity-25"
+            className="bg-indigo-500 hover:bg-indigo-400 text-white py-2 px-5 rounded-r-lg disabled:opacity-25"
+            style={{ minWidth: "87px", minHeight: "52px" }} // Add this line to set a minimum width
             disabled={!isValidISBN(isbn)}
           >
-            Add
+            {isLoading ? (
+              <div role="status">
+                <svg
+                  aria-hidden="true"
+                  className="inline w-5 h-5 animate-spin text-gray-600  fill-white"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              </div>
+            ) : (
+              "Add"
+            )}
           </button>
         </form>
         <br />
@@ -259,22 +277,55 @@ const Homepage = (props: Props) => {
           ) : (
             <p>Valid ISBN.</p>
           )}
-          {bookTitle && <p>Added book: {bookTitle}</p>}
+          {bookTitle ? (
+            <p className="leading-tight">Added book: {bookTitle}</p>
+          ) : (
+            <p className="leading-tight">&nbsp;</p>
+          )}
         </p>
-        {errorMessage && <p>Error: {errorMessage}</p>}
+        {errorMessage ? (
+          <p className="leading-tight">Error: {errorMessage}</p>
+        ) : (
+          <p className="leading-tight">&nbsp;</p>
+        )}
       </section>
       <br />
       {/* <!-- Book Library Section --> */}
       <section className={`mb-8  ${!token ? "hidden" : ""}`}>
         <span className="block sm:flex justify-between mb-8">
           <h3 className="text-xl font-semibold mb-4">Your Library</h3>
-          <input
-            type="text"
-            value={filterText}
-            placeholder="Filter books"
-            onChange={handleFilterChange}
-            className="border border-gray-300 rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          />
+
+          <div className="relative">
+            <input
+              type="text"
+              value={filterText}
+              placeholder="Filter books"
+              onChange={handleFilterChange}
+              className="border border-gray-300 rounded py-2 px-4 pr-12 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            />
+            {filterText && (
+              <button
+                type="button"
+                className="absolute right-2 top-2 text-gray-500 hover:text-gray-700"
+                onClick={() => setFilterText("")}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6 text-gray-900 hover:text-red-500"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+              </button>
+            )}
+          </div>
         </span>
 
         <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
